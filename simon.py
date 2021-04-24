d2444d97175fe2e6ddeb561793fa485004219eca
Joseph Lennox <jlennox@gmail.com>
2021-04-24 09:46:39 -0700

    Move over to using IRQs. I kind of hate it?

--- simon.py
+++ simon.py
@@ -2,11 +2,55 @@ import machine
 import utime
 import random
 
+pressedPin = None
+
+def button_handler(buttonPin):
+    foundPin = None
+    for pin in pins:
+        if pin.button is buttonPin:
+            foundPin = pin
+            break
+
+    if foundPin is None:
+        print("Unable to find button?!")
+        return
+
+    if buttonPin.value() == 1:
+        button_pressed_handler(foundPin)
+    else:
+        button_released_handler(foundPin)
+
+def button_pressed_handler(pin):
+    global pressedPin
+
+    # Prevent weirdness when they push multiple buttons at once.
+    if pressedPin is not None:
+        print("They're already pushing %s!" % pressedPin.name)
+        return
+
+    pressedPin = pin
+    print("User pressed %s" % pin.name)
+
+def button_released_handler(pin):
+    global pressedPin
+
+    if pressedPin is None:
+        print("They released a button but never pushed one?!")
+        return
+
+    # Prevent weirdness when they push multiple buttons at once.
+    if pressedPin is not pin:
+        print("They released %s but pushed %s!" % (pin.name, pressedPin.name))
+        return
+
+    pressedPin = None
+
 class SimonIO:
     def __init__(self, name, ledPin, buttonPin, buzzerFreq):
         self.name = name
         self.led = machine.Pin(ledPin, machine.Pin.OUT)
         self.button = machine.Pin(buttonPin, machine.Pin.IN, machine.Pin.PULL_DOWN)
+        self.button.irq(trigger=machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING, handler=button_handler)
         self.buzzerFreq = buzzerFreq
 
     def buzz(self, enable):
@@ -23,11 +67,13 @@ class SimonIO:
         self.led.off()
         self.buzz(False)
 
+# Configure our program so it knows what pins go where.
+# This is all centralized to keep it from being scattered all over the place.
 pins = [
-    SimonIO("Green", 16, 17, 262), # green, C4
-    SimonIO("Red", 18, 19, 294), # red, D4
-    SimonIO("Yellow", 20, 21, 330), # yellow, E4
-    SimonIO("Blue", 26, 27, 349), # blue, F4
+    SimonIO("Green", 16, 17, 262), # 262 = freq for C4 note. These notes are from the C major scale.
+    SimonIO("Red", 18, 19, 294), # D4
+    SimonIO("Yellow", 20, 21, 330), # E4
+    SimonIO("Blue", 26, 27, 349), # F4
 ]
 
 buzzer = machine.PWM(machine.Pin(28))
@@ -72,29 +118,32 @@ while True:
             entry.show(.5)
             utime.sleep(.2)
 
-        # Now they've got to enter each one.
+        # Now they've got to enter that same sequence.
         for entry in currentGame:
             inputPin = None
 
             # Loop until they enter something or 5 seconds elapses.
             start = utime.time()
-            while inputPin is None and utime.time() - start < 5:
-                for pin in pins:
-                    # Are they pressing that button?
-                    if pin.button.value() == 1:
-                        print("Button %s was pushed..." % pin.name)
-                        pin.buzz(True)
-                        pin.led.on()
-                        inputPin = pin
-                        # Loop until they release the button.
-                        while pin.button.value() == 1:
-                            pass
-                        pin.buzz(False)
-                        pin.led.off()
-                        print("...and released.")
+            # This gets cleared out here. The IRQ handler will be called and
+            # set it once something is entered, breaking our loop.
+            print("Waiting for input...")
+            while pressedPin is None and utime.time() - start < 5:
+                pass
+
+            # Store this because it's going to be overwritten by the IRQ handler
+            wasPressed = pressedPin
+
+            # If they pushed a button, give feedback and loop until they release it.
+            if wasPressed is not None:
+                wasPressed.led.on()
+                wasPressed.buzz(True)
+                while pressedPin is not None:
+                    pass
+                wasPressed.led.off()
+                wasPressed.buzz(False)
 
             # They got it wrong! Or 5 seconds elapsed.
-            if inputPin is not entry:
+            if wasPressed is not entry:
                 print("They lost! Actual answer was %s." % entry.name)
                 # Blink the correct one in their stupid face.
                 entry.buzz(True)
@@ -106,4 +155,5 @@ while True:
                 lost = True
                 break
 
-            utime.sleep(1)
+        # Do a bit of a delay so it doesn't jump right into the replay when they release the button
+        utime.sleep(1)
